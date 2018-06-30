949f00b2c1c625686f4e65e6fe6e641a1000162a
Henrique Costa <henri-kun@outlook.jp>
2018-06-30 20:25:38 -0300

    att
    att

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -155,7 +155,7 @@
 
         targetTmpDate = new Date(
             parameters.year,
-            parameters.month - 1,
+            parameters.month + 1,
             parameters.day,
             parameters.hours,
             parameters.minutes,
